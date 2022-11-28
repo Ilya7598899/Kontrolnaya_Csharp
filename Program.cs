854084e17bcb2d6353fdd63fd6313ad32805c136
Ilya7598899 <114740651+Ilya7598899@users.noreply.github.com>
2022-11-28 17:37:05 +0300

    Создали комментарий в виде описания задачи

--- Program.cs
+++ Program.cs
@@ -4,11 +4,4 @@
 выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 исключительно массивами.
 */
-using System;
-using static System.Console;
 
-Clear();
-
-string[] array = AskArray();
-string[] result = FindLessThan(array, 3);
-WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
