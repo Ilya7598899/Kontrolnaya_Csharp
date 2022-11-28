f727868040407f56ced4a14efe9ab48c54162ceb
Ilya7598899 <114740651+Ilya7598899@users.noreply.github.com>
2022-11-28 17:40:26 +0300

    Создали инициализацию вывода данных (поправка предыдущего коммита)

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ using System;
 using static System.Console;
 
 Clear();
-
 string[] array = AskArray();
 string[] result = FindLessThan(array, 3);
+
+
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
